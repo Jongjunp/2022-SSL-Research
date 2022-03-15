7b39a9b8eb248d9f70d19f2435be6561bb3cfb22
박종준 <parkjongjun@bagjongjuns-MacBook-Air.local>
2022-03-16 00:37:16 +0900

    tester

---
+++ hardware design/task1/tester.sv
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps 
+
+module tester #(
+    parameter A = 8,
+    parameter B = 8,
+    parameter ADDER_0 = A + 1
+)
+(
+    input wire                  ;
+    input 
+);
+
+    initial
+    begin
+
+
+
+    end
+    
